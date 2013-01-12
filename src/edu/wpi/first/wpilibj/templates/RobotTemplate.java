059a76be44b7bdeed3773bd707aa171521de5b3c
William Edward <theelee13@gmail.com>
2013-01-12 14:25:08 -0500

    First Commit

---
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
+
+
+public class RobotTemplate extends SimpleRobot {
+    RobotDrive drive;
+    Joystick left, right;
+
+    public RobotTemplate(){
+        getWatchdog().setEnabled(false);
+        drive=new RobotDrive(1,2);
+        left = new Joystick(1);
+        right = new Joystick(2);
+    }
+    public void autonomous() {
+
+    for (int i = 0; i < 4; i++)
+        {
+           drive.drive(0.5, 0.0); // drive 50% fwd 0% turn
+            Timer.delay(2.0);    
+            drive.drive(0.0, 0.75); // drive 0% fwd, 75% turn
+        }
+    drive.drive(0.0, 0.0); 
+    }
+
+    public void operatorControl() {
+        while(true && isOperatorControl() && isEnabled()){
+            drive.tankDrive(left, right);
+            Timer.delay(.005);
+        }
+    }
+}
