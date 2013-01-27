580a99c1c842dff95afd889d1eb6c49255b2a3fe
Sang-Chan Kim <sck970112@gmail.com>
2013-01-27 13:43:53 -0500

    Updated the Shoot command

--- 4468 Command Robot/src/edu/wpi/first/wpilibj/templates/commands/Shoot.java
+++ 4468 Command Robot/src/edu/wpi/first/wpilibj/templates/commands/Shoot.java
@@ -2,7 +2,8 @@
 package edu.wpi.first.wpilibj.templates.commands;
 import edu.wpi.first.wpilibj.templates.subsystems.*;
 public class Shoot extends CommandBase {
-    
+    private boolean fired = false; 
+	
     public Shoot() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -15,17 +16,21 @@ public class Shoot extends CommandBase {
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-        
+    protected void execute(int frontRPM, int backRPM, double degrees) {
+	    CommandBase.ShooterSubsystem.setShooterSpeed(frontRPM, backRPM); //Starts both motors up
+        CommandBase.LoaderSubsystem.loadFrisbee(degrees); //loads the Frisbee
+		fired = true; //Note: should we add a sensor to make sure the Frisbees do not get jammed? 
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return fired; 
     }
 
     // Called once after isFinished returns true
     protected void end() {
+	    CommandBase.LoaderSubsystem.reset(degrees); //resets the arms back into the original position
+		CommandBase.ShooterSubsystem.setShooterSpeed(0, 0); //stops the motors
     }
 
     // Called when another command which requires one or more of the same
